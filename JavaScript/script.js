2484d52016bc7b98e4e68b3951770d402e3aa779
vlad <vlad.parieiko@mail.ru>
2019-05-29 22:13:35 +0300

    commit 1

---
+++ JavaScript/script.js
@@ -0,0 +1,18 @@
+'use strict';
+/*
+var employeeData = {
+
+	read : function() {
+		this.surName = prompt('Введите вашу фамилию', ''),
+		this.name = prompt('Введите ваше имя', ''),
+		this.patronymic = prompt('Введите ваше отчество', ''),
+		this.age = +prompt('Сколько вам лет', ''),
+		this.salary = +prompt('Какой у вас оклад', ''),
+		this.experienceWork = +prompt('Опыт работы в нашей компании?', '');
+	}
+
+	function getPremiumByAge() {
+		if()
+	}
+*/
+
